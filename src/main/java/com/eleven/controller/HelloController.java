e095d7d7122730c1bfed6ecd2ef8e1ed68efc8fb
CoderJu <1483991357@qq.com>
2017-11-02 21:33:01 +0800

    first commit

---
+++ src/main/java/com/eleven/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.eleven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by User on 2017/11/2.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is welcome page!");
+        model.setViewName("hello");
+        return model;
+
+    }
+}
