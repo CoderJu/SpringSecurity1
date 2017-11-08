c3c314dee12d9216c80b1992f0c00bcb6add68b1
CoderJu <1483991357@qq.com>
2017-11-08 21:13:58 +0800

    Spring Security自定义表单登录实例,由于版本问题，在security里面需要配置login-processing-url=“/j_spring_security_check”

--- src/main/java/com/eleven/controller/HelloController.java
+++ src/main/java/com/eleven/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.eleven.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -12,11 +13,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
 
-        @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
     public ModelAndView welcomePage() {
 
         ModelAndView model = new ModelAndView();
-        model.addObject("title", "Spring Security Hello World");
+        model.addObject("title", "Spring Security login form");
         model.addObject("message", "This is welcome page!");
         model.setViewName("hello");
         return model;
@@ -31,12 +32,17 @@ public class HelloController {
         return model;
     }
 
-    @RequestMapping(value = "/dba**", method = RequestMethod.GET)
-    public ModelAndView dbaPage() {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error",required = false) String error,
+                                @RequestParam(value = "logout",required = false) String logout) {
         ModelAndView model = new ModelAndView();
-        model.addObject("title", "Spring Security Hello World");
-        model.addObject("message", "This is protected dba page - Database Page!");
-        model.setViewName("admin");
+        if (error !=null){
+            model.addObject("error","Invalid username and password!");
+        }
+        if (logout !=null){
+            model.addObject("msg","You've been logged out successfully.");
+        }
+        model.setViewName("login");
         return model;
     }
 
--- src/main/webapp/WEB-INF/page/admin.jsp
+++ src/main/webapp/WEB-INF/page/admin.jsp
@@ -7,6 +7,7 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page session="true" %>
 <html>
 <head>
     <title>Title</title>
@@ -14,11 +15,22 @@
 <body>
 <h1>标题: ${title}</h1>
 <h1>消息 : ${message}</h1>
-
-<c:if test="${pageContext.request.userPrincipal.name != null}">
-    <h2>欢迎: ${pageContext.request.userPrincipal.name}
-        | <a href="<c:url value="/j_spring_security_logout" />" > Logout</a></h2>
-</c:if>
+    <c:url value="/j_spring_security_logout" var="logoutUrl" />
+    <!-- csrt for log out-->
+    <form action="${logoutUrl}" method="post" id="logoutForm">
+        <input type="hidden"  name="${_csrf.parameterName}" value="${_csrf.token}" />
+    </form>
+    <script>
+        function formSubmit() {
+            document.getElementById("logoutForm").submit();
+        }
+    </script>
+    <c:if test="${pageContext.request.userPrincipal.name != null}">
+        <h2>
+            Welcome : ${pageContext.request.userPrincipal.name} | <a
+                href="javascript:formSubmit()"> Logout</a>
+        </h2>
+    </c:if>
 </body>
 
 </html>
